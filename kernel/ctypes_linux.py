632daa7e67fb5af08627906783f4ec0bf9dfa1a5
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2011-07-20 22:02:00 -0400

    cleaning

--- kernel/ctypes_linux.py
+++ kernel/ctypes_linux.py
@@ -7,13 +7,14 @@
 __author__ = "Loic Jaquemet loic.jaquemet+python@gmail.com"
 
 import ctypes
+import logging
+import sys
 
 from haystack import model
 from haystack.model import is_valid_address,getaddress,array2bytes,bytes2array,LoadableMembers
-from haystack.model import RangeValue,NotNull,CString,EVP_CIPHER_CTX_APP_DATA_PTR
+from haystack.model import RangeValue,NotNull,CString
 import ctypes_linux_generated as gen
 
-import logging
 log=logging.getLogger('ctypes_linux')
 
 
@@ -21,17 +22,17 @@ class KernelStruct(LoadableMembers):
   ''' defines classRef '''
   pass
 
-def pasteKernelStructOver(klass):
-  model.pasteLoadableMemberMethodsOn(klass)
-  klass.classRef = KernelStruct.classRef
-  return klass
+################ START copy generated classes ##########################
 
-'''
- We are gonna load all generated structures into local __module__, adding
- LoadableMembers method classes and classRef on them.
- 
- We just have to define expectedValues
-'''
+# copy generated classes (gen.*) to this module as wrapper
+model.copyGeneratedClasses(gen, sys.modules[__name__])
+
+# register all classes (gen.*, locally defines, and local duplicates) to haystack
+# create plain old python object from ctypes.Structure's, to picke them
+model.registerModule(sys.modules[__name__])
+
+################ END   copy generated classes ##########################
+
 
 # replace c_char_p with our String handler
 if type(gen.STRING) != type(CString):
@@ -43,48 +44,15 @@ if type(gen.STRING) != type(CString):
 
 # set expected values 
 gen.task_struct.expectedValues={
-#  'pid': RangeValue(1,65535),
+  'pid': RangeValue(1,65535),
   #'tgid': RangeValue(1,65535),
 #  'flags': RangeValue(1,0xffffffff), #sched.h:1700 , 1 or 2 bits on each 4 bits group
 #  'files': NotNull, # guessing
 #  'fs': NotNull, # guessing
-  #'comm': NotNull, # process name
+#  'comm': NotNull, # process name
 }
 
-
-import inspect,sys
-# auto import gen.* into . ?
-__my_module__ = sys.modules[__name__]
-_loaded=0
-for (name,klass) in inspect.getmembers(gen, inspect.isclass):
-  if type(klass) == type(ctypes.Structure) and 'ctypes_linux_generated' in klass.__module__ :
-    setattr(__my_module__, name, klass)
-    _loaded+=1
-  else:
-    #log.debug("%s - %s"%(name, klass))
-    pass
-log.debug('loaded %d C structs from Kernel structs'%(_loaded))
 log.debug('There is %d members in %s'%(len(gen.__dict__), gen.__name__))
-
-''' Load all generateed classes to local classRef '''
-KernelStruct.classRef=dict([ (ctypes.POINTER( klass), klass) for (name,klass) in inspect.getmembers(sys.modules[__name__], inspect.isclass) if klass.__module__ == __name__ or 'ctypes_linux_generated' in klass.__module__  ])
-
-''' Load all model classes and create a similar non-ctypes Python class  
-  thoses will be used to translate non pickable ctypes into POPOs.
-'''
-for klass,typ in inspect.getmembers(sys.modules[__name__], inspect.isclass):
-  if typ.__module__.startswith(__name__):
-    kpy = type('%s_py'%(klass),(model.pyObj,),{})
-    setattr(sys.modules[__name__], '%s_py'%(klass), kpy )
-    if typ.__module__ != __name__: # class is ctypes_xxx_generated
-      setattr(sys.modules[typ.__module__], '%s_py'%(klass), kpy )
-      #log.debug("Created %s_py"%klass)
-
-# copy classRef and methods, we have to wait after all class are loaded and in NSSStruct.classRef
-for (name,klass) in inspect.getmembers(gen, inspect.isclass):
-  if type(klass) == type(ctypes.Structure) and 'ctypes_linux_generated' in klass.__module__  :
-    pasteKernelStructOver(klass)
-    #log.debug("painted on %s"%klass)
 
 
 
@@ -94,8 +62,6 @@ def printSizeof(mini=-1):
     if type(klass) == type(ctypes.Structure) and 'ctypes_linux_generated' in klass.__module__  :
       if ctypes.sizeof(klass) > mini:
         print '%s:'%name,ctypes.sizeof(klass)
-  #print 'SSLCipherSuiteInfo:',ctypes.sizeof(SSLCipherSuiteInfo)
-  #print 'SSLChannelInfo:',ctypes.sizeof(SSLChannelInfo)
 
 if __name__ == '__main__':
   printSizeof(200)
